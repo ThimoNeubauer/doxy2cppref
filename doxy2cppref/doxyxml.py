f78db060caa728979e3144e5a300366fb2342691
Thimo Neubauer <thimo@macht.org>
2016-05-10 21:38:54 -0600

    Extremely simple command line, write separate files

--- doxy2cppref/doxyxml.py
+++ doxy2cppref/doxyxml.py
@@ -33,7 +33,7 @@ class DoxyIndex:
         return result
 
     def class_details(self, classinfo):
-        return DoxyClass(os.path.join(self.basedir, classinfo.refid + ".xml"))
+        return DoxyClass(classinfo.cppname, os.path.join(self.basedir, classinfo.refid + ".xml"))
 
 
 class DocThing:
@@ -49,22 +49,30 @@ class DocThing:
 
 # TODO: use the <para> elements and such
 def doxy2wiki(description):
-    return " ".join(description.itertext())
+    return " ".join(description.itertext()).strip()
 
 
 class DoxyClass:
 
-    def __init__(self, filename: str):
+    def __init__(self, cppname, filename: str):
+        self.cppname = cppname
+
         tree = ElementTree.parse(filename)
         self.root = tree.getroot()
 
     def name(self):
         return self.root.find('.//compoundname').text
 
-    def methods(self):
+    def variables(self):
+        return self.__memberdefs("[@kind='variable']")
+
+    def public_members(self):
+        return self.__memberdefs("[@kind='function'][@prot='public']")
+
+    def __memberdefs(self, selector):
         results = list()
 
-        for memberdef in self.root.findall('.//memberdef'):
+        for memberdef in self.root.findall('.//memberdef' + selector):
             dc = DocThing()
 
             dc.what = memberdef.find('definition').text
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 #  -*- encoding: utf-8 -*-
 
+import argparse
 import logging
+import os
 
 from doxy2cppref.doxyxml import DoxyIndex
 
@@ -9,27 +11,69 @@ from doxy2cppref.doxyxml import DoxyIndex
 logger = logging.getLogger("doxy2cppref")
 
 
-def render_page(title, docthings):
+class MyError(Exception):
+    pass
 
-    print("{{cpp/title|%s}}" % title)
 
-    print("===Member functions===")
-    print("{{dsc begin}}")
+def render_class(class_details, outdir):
+    cppname = class_details.cppname
 
-    for doc in [doc for doc in docthings if doc.kind == "function"]:
-        print(doc.what)
+    filename = os.path.join(outdir, cppname)
+    logger.info("Writing {}".format(filename))
+    overview_file = open(filename, "w", encoding="utf-8")
+    overview_file.write("{{cpp/title|%s}}\n\n" % cppname)
 
-    print("{{dsc end}}")
+    members = class_details.public_members()
+    if len(members) > 0:
+        overview_file.write("=== Member functions ===\n\n")
+        overview_file.write("{{dsc begin}}\n")
+
+        for member in members:
+            overview_file.write("{{dsc mem fun | missing | %s}}\n" % member.brief)
+
+        overview_file.write("{{dsc end}}\n\n")
+
+    overview_file.close()
 
 
 def main():
-    index = DoxyIndex('example/xml/index.xml')
+    # -- setup
+
+    parser = argparse.ArgumentParser(description='Process data storage definition files.')
+    parser.add_argument('-d', '--debug', action='store_true', help='activate more output')
+    parser.add_argument('indexfile', type=str, help="Doxygen index.xml")
+    parser.add_argument('outdir', type=str, help="output directory")
+
+    args = parser.parse_args()
+
+    if args.debug:
+        level = logging.DEBUG
+    else:
+        level = logging.INFO
+
+    logging.basicConfig(level=level, format="%(message)s")
+
+    indexfile = args.indexfile
+    if not os.path.isfile(indexfile):
+        raise MyError("Could not find index file {}!".format(indexfile))
+
+    outdir = args.outdir
+    if os.path.isdir(outdir):
+        logger.warn("Will re-generate {} directory now".format(outdir))
+    if not os.path.isdir(outdir):
+        logger.info("Creating {}".format(outdir))
+        os.makedirs(outdir)
+
+    # -- operation
+
+    index = DoxyIndex(indexfile)
     for cls in index.classes():
         class_details = index.class_details(cls)
-
-        render_page(class_details.name(), class_details.methods())
+        render_class(class_details, outdir)
 
 try:
     main()
+except MyError as ex:
+    logger.error(ex)
 except Exception as ex:
     logging.exception(ex)
