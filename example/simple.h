7615a8e90e2c3801bca828a8ee4a74d1c0b92470
Thimo Neubauer <thimo@macht.org>
2016-05-11 14:02:54 -0600

    Update the example to with brief/full description of class, a public typedef and a template parameter

--- example/simple.h
+++ example/simple.h
@@ -2,8 +2,19 @@
 
 namespace Simple {
 
+    /*! @brief Does something
+     *
+     * In detail the Whatever class still doesn't do anything useful
+     *
+     * \tparam T1 fulfills the concept of a type
+     */
+
+    template <typename T1>
     class Whatever {
     public:
+      //! public typedef
+      typedef MyInt int;
+
       //! the trivial constructor
       Whatever();
 
